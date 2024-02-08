22b2ec9883448513d3d3fa9498bfd418b1ac7f02
brentgroves <brent.groves@gmail.com>
2024-02-08 16:40:07 -0500

    updated source code

--- prod/build_deploy_run/run-tb.md
+++ prod/build_deploy_run/run-tb.md
@@ -1,5 +1,6 @@
 # **Format of file name**
 
+TB-202301_to_202401_on_02-08_DM
 TB-202212_to_202312_on_01-09_DM
 TB-202212_to_202312_on_01-03_DM_LR
 TB-202211_to_202311_on_12-12_DM_LR_JS
@@ -149,6 +150,7 @@ Thank you Father for the peace that you have given me in pain and sorrow!
 From teams run the TB report and export it for the requested period range using date format 202210.
 
 **Format of file name**
+TB-202301_to_202401_on_02-08_DM
 TB-202212_to_202312_on_01-03_DM_LR
 TB-202211_to_202311_on_12-12_DM_LR_JS
 TB-202211_to_202311_on_12-08_DM_LR_JS
--- prod/volume/PipeLine/TrialBalance-test.sh
+++ prod/volume/PipeLine/TrialBalance-test.sh
@@ -241,17 +241,17 @@ tm=""
 # set pcn
 pcn=123681
 
-if [[ $result -eq 0 ]]
-then # if/then branch
-  exec 6<>tm-msg
-  read input <&6 && echo "$script time: ${input}" 1>&4
-  exec 6<>tm-msg
-  script="AccountingBalanceAppendPeriodRange"
-  printf "\nStarting: $script\n" 1>&4
-  cd ../AccountingBalanceAppendPeriodRange
-  source AccountingBalanceAppendPeriodRange.sh 
-  printf "\n$script result=$result"
-fi
+# if [[ $result -eq 0 ]]
+# then # if/then branch
+#   exec 6<>tm-msg
+#   read input <&6 && echo "$script time: ${input}" 1>&4
+#   exec 6<>tm-msg
+#   script="AccountingBalanceAppendPeriodRange"
+#   printf "\nStarting: $script\n" 1>&4
+#   cd ../AccountingBalanceAppendPeriodRange
+#   source AccountingBalanceAppendPeriodRange.sh 
+#   printf "\n$script result=$result"
+# fi
 
 # # reset variables
 # em=""
@@ -279,17 +279,17 @@ tm=""
 # set pcn
 pcn=123681
 # https://askubuntu.com/questions/1403837/how-do-i-use-openssl-1-1-1-in-ubuntu-22-04
-if [[ $result -eq 0 ]]
-then # if/then branch
-  exec 6<>tm-msg
-  read input <&6 && echo "$script time: ${input}" 1>&4
-  exec 6<>tm-msg
-  script="AccountActivitySummaryGetOpenPeriodRange"
-  printf "\nStarting: $script\n" 1>&4
-  cd ../AccountActivitySummaryGetOpenPeriodRange
-  source AccountActivitySummaryGetOpenPeriodRange.sh 
-  printf "\n$script result=$result"
-fi
+# if [[ $result -eq 0 ]]
+# then # if/then branch
+#   exec 6<>tm-msg
+#   read input <&6 && echo "$script time: ${input}" 1>&4
+#   exec 6<>tm-msg
+#   script="AccountActivitySummaryGetOpenPeriodRange"
+#   printf "\nStarting: $script\n" 1>&4
+#   cd ../AccountActivitySummaryGetOpenPeriodRange
+#   source AccountActivitySummaryGetOpenPeriodRange.sh 
+#   printf "\n$script result=$result"
+# fi
 
 # # # reset variables
 # # em=""
@@ -319,17 +319,17 @@ tm=""
 # set pcn
 # pcn=123681
 
-# if [[ $result -eq 0 ]]
-# then # if/then branch
-#   exec 6<>tm-msg
-#   read input <&6 && echo "$script time: ${input}" 1>&4
-#   exec 6<>tm-msg
-#   script="AccountPeriodBalanceRecreatePeriodRange"
-#   printf "\nStarting: $script\n" 1>&4
-#   cd ../AccountPeriodBalanceRecreatePeriodRange
-#   source AccountPeriodBalanceRecreatePeriodRange.sh 
-#   printf "\n$script result=$result"
-# fi
+if [[ $result -eq 0 ]]
+then # if/then branch
+  exec 6<>tm-msg
+  read input <&6 && echo "$script time: ${input}" 1>&4
+  exec 6<>tm-msg
+  script="AccountPeriodBalanceRecreatePeriodRange"
+  printf "\nStarting: $script\n" 1>&4
+  cd ../AccountPeriodBalanceRecreatePeriodRange
+  source AccountPeriodBalanceRecreatePeriodRange.sh 
+  printf "\n$script result=$result"
+fi
 
 # # reset variables
 # em=""
@@ -365,17 +365,17 @@ pcn=123681
 # WATCH: THERE WAS AN ERROR IN THIS SCRIPT FOR MYSQL THERE WAS A 202213 PERIOD  
 # "AccountPeriodBalanceRecreateOpenPeriodRange"
 
-# if [[ $result -eq 0 ]]
-# then # if/then branch
-#   exec 6<>tm-msg
-#   read input <&6 && echo "$script time: ${input}" 1>&4
-#   exec 6<>tm-msg
-#   script="AccountPeriodBalanceRecreateOpenPeriodRange"
-#   printf "\nStarting: $script\n" 1>&4
-#   cd ../AccountPeriodBalanceRecreateOpenPeriodRange
-#   source AccountPeriodBalanceRecreateOpenPeriodRange.sh 
-#   printf "\n$script result=$result"
-# fi
+if [[ $result -eq 0 ]]
+then # if/then branch
+  exec 6<>tm-msg
+  read input <&6 && echo "$script time: ${input}" 1>&4
+  exec 6<>tm-msg
+  script="AccountPeriodBalanceRecreateOpenPeriodRange"
+  printf "\nStarting: $script\n" 1>&4
+  cd ../AccountPeriodBalanceRecreateOpenPeriodRange
+  source AccountPeriodBalanceRecreateOpenPeriodRange.sh 
+  printf "\n$script result=$result"
+fi
 
 
 # # # reset variables
